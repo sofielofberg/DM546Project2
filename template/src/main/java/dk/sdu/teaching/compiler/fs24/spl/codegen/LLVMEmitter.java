67a4dc53aca74f6af21904aa15f18d2d75046830
Sofie Løfberg <soloe23@student.sdu.dk>
2025-10-28 10:39:04 +0100

    first commit - added template

---
+++ template/src/main/java/dk/sdu/teaching/compiler/fs24/spl/codegen/LLVMEmitter.java
@@ -0,0 +1,15 @@
+package dk.sdu.teaching.compiler.fs24.spl.codegen;
+
+import java.util.List;
+
+import dk.sdu.teaching.compiler.fs24.spl.ast.Stmt;
+
+
+public class LLVMEmitter {
+    
+    public void generateCode(List<Stmt> statements) {
+        throw new UnsupportedOperationException("Unimplemented method 'generateCode'");
+    }
+
+  
+}
